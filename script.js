7a94e981e68a99d183b58297bd33f2aca0d6efa1
Felipe <felipejsilvacosta@gmail.com>
2020-09-30 18:57:44 +1000

    New features - paint symbol when have a winner, count the score, new game button, CSS animation and fixed some bugs

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 // variable
 let boxes = document.querySelectorAll(".box");
-let player1Result = document.querySelector(".player1-result");
-let player2Result = document.querySelector(".player2-result");
+let playerResult = document.querySelector(".player-result");
+let player1Light = document.querySelector(".player1-light").style;
+let player2Light = document.querySelector(".player2-light").style;
+let btnReset = document.querySelector(".btn-reset");
+let player1Score = document.querySelector(".player1-score-total");
+let player2Score = document.querySelector(".player2-score-total");
 let player1Symbol = "X";
 let player2Symbol = "O";
 let startPlayer = true;
+let player1Total = 1;
+let player2Total = 1;
 let result = ["", "", "", "", "", "", "", "", ""];
-let player1Light = document.querySelector(".player1-light").style;
-let player2Light = document.querySelector(".player2-light").style;
 
 // events
 boxes.forEach((element) => {
   element.addEventListener("click", changeBox);
 });
+btnReset.addEventListener("click", newGame);
 player1Light.visibility = "visible";
+
 // methods
 function changeBox(event) {
   let index = 0;
   let element = 0;
-
-  player1Result.textContent = "";
-  player2Result.textContent = "";
 
   if (startPlayer) {
     event.target.textContent = player1Symbol;
@@ -33,6 +36,7 @@ function changeBox(event) {
     player2Light.visibility = "hidden";
     player1Light.visibility = "visible";
   }
+
   index = event.target.dataset.value;
   element = event.target.textContent;
   result.splice(index, 1, element);
@@ -45,44 +49,52 @@ function changeBox(event) {
 
 function checkColumns(player) {
   if (result[0] == player && result[6] == player && result[3] == player) {
+    drawLine(0, 6, 3);
     winner(player);
   }
   if (result[1] == player && result[7] == player && result[4] == player) {
+    drawLine(1, 7, 4);
     winner(player);
   }
   if (result[2] == player && result[8] == player && result[5] == player) {
+    drawLine(2, 8, 5);
     winner(player);
   }
 }
 
 function checkRows(player) {
   if (result[0] == player && result[1] == player && result[2] == player) {
+    drawLine(0, 1, 2);
     winner(player);
   }
   if (result[3] == player && result[4] == player && result[5] == player) {
+    drawLine(3, 4, 5);
     winner(player);
   }
   if (result[6] == player && result[7] == player && result[8] == player) {
+    drawLine(6, 7, 8);
     winner(player);
   }
 }
 
 function checkDiagonal(player) {
   if (result[0] == player && result[4] == player && result[8] == player) {
+    drawLine(0, 4, 8);
     winner(player);
   }
   if (result[2] == player && result[4] == player && result[6] == player) {
+    drawLine(2, 4, 6);
     winner(player);
   }
 }
 
 function checkDraw() {
   if (result.indexOf("") == -1) {
-    player1Result.textContent = `DRAW`;
-    player2Result.textContent = `DRAW`;
+    playerResult.textContent = `DRAW`;
     player1Light.visibility = "hidden";
     player2Light.visibility = "hidden";
     startPlayer = true;
+    btnReset.style.visibility = "visible";
     boxes.forEach((element) => {
       element.textContent = "";
       result[element.dataset.value] = "";
@@ -91,18 +103,34 @@ function checkDraw() {
 }
 
 function winner(player) {
-  boxes.forEach((element) => {
-    element.textContent = "";
-    result[element.dataset.value] = "";
-  });
-  startPlayer = true;
   if (player == "X") {
-    player1Result.textContent = `PLAYER 1 WON`;
+    playerResult.textContent = `PLAYER 1 WON`;
+    player1Score.textContent = player1Total++;
     player1Light.visibility = "visible";
     player2Light.visibility = "hidden";
+    btnReset.style.visibility = "visible";
   } else {
-    player2Result.textContent = `PLAYER 2 WON`;
+    playerResult.textContent = `PLAYER 2 WON`;
+    player2Score.textContent = player2Total++;
     player1Light.visibility = "hidden";
     player2Light.visibility = "visible";
+    btnReset.style.visibility = "visible";
   }
+}
+
+function drawLine(...args) {
+  args.forEach((elem) => {
+    boxes[elem].style.color = "green";
+  });
+}
+
+function newGame() {
+  boxes.forEach((element) => {
+    element.textContent = "";
+    element.style.color = "white";
+    result[element.dataset.value] = "";
+  });
+  playerResult.textContent = "";
+  startPlayer = true;
+  btnReset.style.visibility = "hidden";
 }
