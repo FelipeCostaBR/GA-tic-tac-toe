9fdcd902e3efcbd8c8c35d85754e60530a22f92e
Felipe <felipejsilvacosta@gmail.com>
2020-10-02 09:52:10 +1000

    New features - reset button, created localStorage, set player name, CSS animation and fixed bugs

--- script.js
+++ script.js
@@ -1,136 +1,241 @@
 // variable
 let boxes = document.querySelectorAll(".box");
-let playerResult = document.querySelector(".player-result");
+let btnReset = document.querySelector(".reset");
+let container = document.querySelector(".game-board");
 let player1Light = document.querySelector(".player1-light").style;
 let player2Light = document.querySelector(".player2-light").style;
-let btnReset = document.querySelector(".btn-reset");
+let player1 = document.querySelector(".player1");
+let player2 = document.querySelector(".player2");
 let player1Score = document.querySelector(".player1-score-total");
 let player2Score = document.querySelector(".player2-score-total");
 let player1Symbol = "X";
 let player2Symbol = "O";
-let startPlayer = true;
-let player1Total = 1;
-let player2Total = 1;
+let player1Total = 0;
+let player2Total = 0;
+let startPlayer = true; // to start with O set to false
 let result = ["", "", "", "", "", "", "", "", ""];
+let doDraw = true;
+let fixDoubleScore = false;
 
 // events
 boxes.forEach((element) => {
   element.addEventListener("click", changeBox);
 });
-btnReset.addEventListener("click", newGame);
-player1Light.visibility = "visible";
+btnReset.addEventListener("click", function () {
+  localStorage.clear();
+  player1Score.textContent = "0";
+  player2Score.textContent = "0";
+  player1Total = 1;
+  player2Total = 1;
+  player1.textContent = "";
+  player2.textContent = "";
+  selectNames();
+});
+VanillaTilt.init(document.querySelector(".js-tilt"));
 
 // methods
 function changeBox(event) {
-  let index = 0;
-  let element = 0;
+  let arrayIndex = 0;
+  let symbol = 0;
+  arrayIndex = event.target.dataset.value;
 
-  if (startPlayer) {
-    event.target.textContent = player1Symbol;
-    startPlayer = false;
-    player1Light.visibility = "hidden";
-    player2Light.visibility = "visible";
-  } else {
-    event.target.textContent = player2Symbol;
-    startPlayer = true;
-    player2Light.visibility = "hidden";
-    player1Light.visibility = "visible";
+  if (result[arrayIndex] == "") {
+    if (startPlayer) {
+      event.target.textContent = player1Symbol;
+      event.target.classList.add("flip-horizontal-bottom");
+      startPlayer = false;
+      player1Light.visibility = "hidden";
+      player2Light.visibility = "visible";
+    } else {
+      event.target.textContent = player2Symbol;
+      event.target.classList.add("flip-horizontal-bottom");
+      startPlayer = true;
+      player2Light.visibility = "hidden";
+      player1Light.visibility = "visible";
+    }
+
+    symbol = event.target.textContent;
+    result.splice(arrayIndex, 1, symbol);
+
+    checkColumns(symbol);
+    checkRows(symbol);
+    checkDiagonal(symbol);
+    checkDraw();
   }
-
-  index = event.target.dataset.value;
-  element = event.target.textContent;
-  result.splice(index, 1, element);
-
-  checkColumns(element);
-  checkRows(element);
-  checkDiagonal(element);
-  checkDraw();
 }
 
 function checkColumns(player) {
   if (result[0] == player && result[6] == player && result[3] == player) {
-    drawLine(0, 6, 3);
-    winner(player);
-  }
-  if (result[1] == player && result[7] == player && result[4] == player) {
-    drawLine(1, 7, 4);
-    winner(player);
-  }
-  if (result[2] == player && result[8] == player && result[5] == player) {
-    drawLine(2, 8, 5);
-    winner(player);
+    winner(player, fixDoubleScore);
+    fixDoubleScore = drawLine(0, 6, 3);
+  } else if (
+    result[1] == player &&
+    result[7] == player &&
+    result[4] == player
+  ) {
+    winner(player, fixDoubleScore);
+    fixDoubleScore = drawLine(1, 7, 4);
+  } else if (
+    result[2] == player &&
+    result[8] == player &&
+    result[5] == player
+  ) {
+    winner(player, fixDoubleScore);
+    fixDoubleScore = drawLine(2, 8, 5);
   }
 }
 
 function checkRows(player) {
   if (result[0] == player && result[1] == player && result[2] == player) {
-    drawLine(0, 1, 2);
-    winner(player);
-  }
-  if (result[3] == player && result[4] == player && result[5] == player) {
-    drawLine(3, 4, 5);
-    winner(player);
-  }
-  if (result[6] == player && result[7] == player && result[8] == player) {
-    drawLine(6, 7, 8);
-    winner(player);
+    winner(player, fixDoubleScore);
+    fixDoubleScore = drawLine(0, 1, 2);
+  } else if (
+    result[3] == player &&
+    result[4] == player &&
+    result[5] == player
+  ) {
+    winner(player, fixDoubleScore);
+    fixDoubleScore = drawLine(3, 4, 5);
+  } else if (
+    result[6] == player &&
+    result[7] == player &&
+    result[8] == player
+  ) {
+    winner(player, fixDoubleScore);
+    fixDoubleScore = drawLine(6, 7, 8);
   }
 }
 
 function checkDiagonal(player) {
   if (result[0] == player && result[4] == player && result[8] == player) {
-    drawLine(0, 4, 8);
-    winner(player);
-  }
-  if (result[2] == player && result[4] == player && result[6] == player) {
-    drawLine(2, 4, 6);
-    winner(player);
+    winner(player, fixDoubleScore);
+    fixDoubleScore = drawLine(0, 4, 8);
+  } else if (
+    result[2] == player &&
+    result[4] == player &&
+    result[6] == player
+  ) {
+    winner(player, fixDoubleScore);
+    fixDoubleScore = drawLine(2, 4, 6);
   }
 }
 
 function checkDraw() {
-  if (result.indexOf("") == -1) {
-    playerResult.textContent = `DRAW`;
-    player1Light.visibility = "hidden";
-    player2Light.visibility = "hidden";
-    startPlayer = true;
-    btnReset.style.visibility = "visible";
-    boxes.forEach((element) => {
-      element.textContent = "";
-      result[element.dataset.value] = "";
-    });
-  }
-}
-
-function winner(player) {
-  if (player == "X") {
-    playerResult.textContent = `PLAYER 1 WON`;
-    player1Score.textContent = player1Total++;
-    player1Light.visibility = "visible";
-    player2Light.visibility = "hidden";
-    btnReset.style.visibility = "visible";
-  } else {
-    playerResult.textContent = `PLAYER 2 WON`;
-    player2Score.textContent = player2Total++;
-    player1Light.visibility = "hidden";
-    player2Light.visibility = "visible";
-    btnReset.style.visibility = "visible";
-  }
+  boxes.forEach((elem) => {
+    if (elem.style.color == "green") {
+      doDraw = false;
+    } else if (result.indexOf("") == -1 && doDraw) {
+      player1Light.visibility = "hidden";
+      player2Light.visibility = "hidden";
+      startPlayer = true;
+      container.classList.add("game-board-size");
+      swal(`DRAW`, `lets play again`).then(() => {
+        newGame();
+      });
+    }
+  });
 }
 
 function drawLine(...args) {
   args.forEach((elem) => {
     boxes[elem].style.color = "green";
   });
+  return true;
+}
+
+function winner(player, fixDoubleScore) {
+  player1Light.visibility = "hidden";
+  player2Light.visibility = "hidden";
+  container.classList.add("game-board-size");
+
+  if (player == "X" && !fixDoubleScore) {
+    player1Score.textContent = player1Total++;
+    localStorage.setItem("scorePlayer1", player1Score.textContent);
+
+    swal(`${player1.textContent.toUpperCase()} WON`, `lets play again`).then(
+      () => {
+        newGame();
+      }
+    );
+  } else if (!fixDoubleScore) {
+    player2Score.textContent = player2Total++;
+    localStorage.setItem("scorePlayer2", player2Score.textContent);
+
+    swal(`${player2.textContent.toUpperCase()} WON`, `lets play again`).then(
+      () => {
+        newGame();
+      }
+    );
+  }
 }
 
 function newGame() {
   boxes.forEach((element) => {
     element.textContent = "";
-    element.style.color = "white";
+    element.style.color = "";
     result[element.dataset.value] = "";
+    element.classList.remove("flip-horizontal-bottom");
   });
-  playerResult.textContent = "";
   startPlayer = true;
-  btnReset.style.visibility = "hidden";
+  player1Light.visibility = "visible";
+  player2Light.visibility = "hidden";
+  doDraw = true;
+  fixDoubleScore = false;
+  container.classList.remove("game-board-size");
 }
+
+function selectNames() {
+  swal({
+    title: "CHOSE A NICE NAME",
+    text: "Write the name of the first player:",
+    buttons: true,
+    closeModal: true,
+    content: {
+      element: "input",
+      attributes: {
+        placeholder: "type your name",
+      },
+    },
+  }).then(function (value) {
+    player1.textContent = value;
+    localStorage.setItem("namePlayer1", player1.textContent);
+    swal({
+      title: "CHOSE A NICE NAME",
+      text: "Write the name of the second player:",
+      buttons: true,
+      closeModal: true,
+      content: {
+        element: "input",
+        attributes: {
+          placeholder: "type your name",
+        },
+      },
+    }).then(function (value) {
+      player2.textContent = value;
+      localStorage.setItem("namePlayer2", player2.textContent);
+      swal("Nice!", "let's play", "success");
+      player1Light.visibility = "visible";
+    });
+  });
+}
+
+function storageData() {
+  if (
+    localStorage.getItem("scorePlayer1") ||
+    localStorage.getItem("scorePlayer2")
+  ) {
+    player1Total = Number(localStorage.getItem("scorePlayer1")) + 1;
+    player1Score.textContent = localStorage.getItem("scorePlayer1");
+    player1.textContent = localStorage.getItem("namePlayer1");
+    player2Total = Number(localStorage.getItem("scorePlayer2")) + 1;
+    player2Score.textContent = localStorage.getItem("scorePlayer2");
+    player2.textContent = localStorage.getItem("namePlayer2");
+    player1Light.visibility = "visible";
+  } else {
+    player1Total = Number(localStorage.getItem("scorePlayer1")) + 1;
+    player2Total = Number(localStorage.getItem("scorePlayer2")) + 1;
+    selectNames();
+  }
+}
+
+storageData();
